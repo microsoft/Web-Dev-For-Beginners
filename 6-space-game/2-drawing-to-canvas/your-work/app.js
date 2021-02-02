b3a64cf551788e56f8e434d9ab0bfa4bf66773da
Roberto Pauletto <roberto.pauletto@gmail.com>
2021-02-02 16:46:05 +0100

    Ch. 6 - main readme plus sub chapters 1, 2, 3 translated

--- 6-space-game/2-drawing-to-canvas/your-work/app.js
+++ 6-space-game/2-drawing-to-canvas/your-work/app.js
@@ -1,24 +1,24 @@
 function loadTexture(path) {
-	return new Promise((resolve) => {
-		const img = new Image();
-		img.src = path;
-		img.onload = () => {
-			resolve(img);
-		};
-	});
+  return new Promise((resolve) => {
+    const img = new Image()
+    img.src = path
+    img.onload = () => {
+      resolve(img)
+    }
+  })
 }
 
 function createEnemies(ctx, canvas, enemyImg) {
-	// TODO draw enemies
+  // TODO disegnare i nemici
 }
 
 window.onload = async () => {
-	canvas = document.getElementById('canvas');
-	ctx = canvas.getContext('2d');
-	// TODO load textures
+  canvas = document.getElementById('canvas')
+  ctx = canvas.getContext('2d')
+  // TODO 2 - caricare le strutture di gioco
 
-	// TODO draw black background
-	// TODO draw hero
-	// TODO uncomment the next line when you add enemies to screen
-	//createEnemies(ctx, canvas, enemyImg);
-};
+  // TODO 1 - disegnare lo sfondo nero
+  // TODO 3 - disegnare l'eroe
+  // TODO 4 - togliere il commento dalla riga successiva quando si aggiungono nemici sullo schermo
+  //createEnemies(ctx, canvas, enemyImg);
+}
