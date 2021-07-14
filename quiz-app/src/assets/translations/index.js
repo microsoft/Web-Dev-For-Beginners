4599d33092cd77ebfd6fcc7d6eafbf794796444b
Jen Looper <jen.looper@gmail.com>
2021-07-14 08:17:29 -0400

    fixing the build for the quiz-app

--- quiz-app/src/assets/translations/index.js
+++ quiz-app/src/assets/translations/index.js
@@ -1,36 +1,34 @@
 // index.js
-import en from './en.json';
-import ko from './ko.json';
-import id from './id.json';
-import hi from './hi.json';
-import it from './it.json';
-import ja from './ja.json';
-import gr from './gr.json';
-import ms from './ms.json';
-import es from './es.json';
-import nl from './nl.json';
-import zh_cn from './zh_cn.json';
-import zh_tw from './zh_tw.json';
-import fr from './fr.json';
-
+import en from "./en.json";
+import ko from "./ko.json";
+import id from "./id.json";
+import hi from "./hi.json";
+import it from "./it.json";
+import ja from "./ja.json";
+import gr from "./gr.json";
+import ms from "./ms.json";
+import es from "./es.json";
+import nl from "./nl.json";
+import zh_cn from "./zh_cn.json";
+import zh_tw from "./zh_tw.json";
+import fr from "./fr.json";
 
 //export const defaultLocale = 'en';
 
 const messages = {
-	en: en[0],
-	ko: ko[0],
-	id: id[0],
-	hi: hi[0],
-	it: it[0],
-	ja: ja[0],
-	gr: gr[0],
-	ms: ms[0],
-	es: es[0],
-	nl: nl[0],
-	zh_cn: zh_cn[0],
+  en: en[0],
+  ko: ko[0],
+  id: id[0],
+  hi: hi[0],
+  it: it[0],
+  ja: ja[0],
+  gr: gr[0],
+  ms: ms[0],
+  es: es[0],
+  nl: nl[0],
+  zh_cn: zh_cn[0],
   zh_tw: zh_tw[0],
-  	fr: fr[0],
-
+  fr: fr[0],
 };
 
 export default messages;
