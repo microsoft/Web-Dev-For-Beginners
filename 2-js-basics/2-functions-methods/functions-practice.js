865fafd6067cd4359d75c9006a003f3878b545ef
Amir Walker <amirwalker@ucmerced.edu>
2026-08-27 14:05:48 -0700

    Added function to calc sum of 2 numbers

--- 2-js-basics/2-functions-methods/functions-practice.js
+++ 2-js-basics/2-functions-methods/functions-practice.js
@@ -1,13 +1,27 @@
 
-function sayHello(){
+function sayHello()
+{
     console.Log("Hello");
 }
 
 
-function introduceYourself(name){
+function introduceYourself(name)
+{
     console.Log(`Hi, my name is ${name}`);
 }
 
-function greetPerson(name, greeting='Hello' ){
+function greetPerson(name, greeting='Hello' )
+{
     console.Log(`${greeting}, ${name}!`);
 }
+
+// setTimeout(() => {
+  // console.log('3 seconds has elapsed');
+// }, 3000);
+
+function addNumbers(num1, num2)
+{
+ sum = num1 + num2;
+ return sum;
+}
+
