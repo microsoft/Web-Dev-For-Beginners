df8dcccd555d5de77099977cc2d7ab36dbb3cfac
Amir Walker <amirwalker@ucmerced.edu>
2026-08-27 14:01:33 -0700

    Wrote greetPerson

--- 2-js-basics/2-functions-methods/functions-practice.js
+++ 2-js-basics/2-functions-methods/functions-practice.js
@@ -4,6 +4,10 @@ function sayHello(){
 }
 
 
-function introductYourself(name){
-    console.Log(`Hi, my name is ${name}`)
+function introduceYourself(name){
+    console.Log(`Hi, my name is ${name}`);
+}
+
+function greetPerson(name, greeting='Hello' ){
+    console.Log(`${greeting}, ${name}!`);
 }
